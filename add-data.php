9cbf24a8fa25edee93150ad2edc831783b475052
Daphne Jimmy <94054467+DaphneJimmy@users.noreply.github.com>
2023-01-18 23:47:31 +0800

    Create add-data.php

---
+++ add-data.php
@@ -0,0 +1,41 @@
+<?php
+include('layouts/header.php');
+?>
+
+<main class="container mt-5">
+
+<div class="mb-3"></n>
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+
+<select class="form-select" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+
+<p><form class="row g-3"></p>
+  <div class="col-auto">
+    <label for="staticEmail2" class="visually-hidden">Email</label>
+    <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com">
+  </div>
+  <div class="col-auto">
+    <label for="inputPassword2" class="visually-hidden">Password</label>
+    <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
+  </div>
+  <div class="col-auto">
+    <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
+  </div>
+</form>
+
+</main>
+
+<?php
+include('layouts/footer.php');
+?>
